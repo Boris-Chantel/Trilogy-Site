5c6b93e98f7846b495274b9ea896b3307a9eee41
Boris-Chantel <you@example.com>
2016-05-23 19:52:44 -0400

    fixed header added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
-$('a').on('click', function() {
-	var target = $(this).attr('href');
+var init = function() {
+    $('a').on('click', function() {
+        var target = $(this).attr('href');
 
-	$('body').stop().animate({
-		scrollTop: $(target).offset().top -30}, 1000);
-});
+        $('body').stop().animate({
+            scrollTop: $(target).offset().top - 90
+        }, 1000);
+    });
+    $(window).on('load scroll', setHeader);
+
+};
+
+var setHeader = function() {
+    var y = $(window).scrollTop();
+
+    if (y > 300) {
+    	$('nav').addClass('scroll');
+    } else $('nav').removeClass('scroll'); 
+    console.log(y);
+    
+
+
+};
+
+
+init();
